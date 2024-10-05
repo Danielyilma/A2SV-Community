a9bf189540d4c8980a515c6f6a88513f9880faff
Danielyilma <105011761+Danielyilma@users.noreply.github.com>
2024-10-05 11:49:16 +0300

    set_difference

---
+++ set_difference.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+no_english = int(input())
+english_sub = set(map(int, input().split()))
+no_french = int(input())
+french_sub = set(map(int, input().split()))
+
+print(len(english_sub - french_sub))
